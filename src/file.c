cce265b0ad213a81b7d97777c4cc9439b07f9302
DevDaddyDownUnder <gossip_ponchos0g@icloud.com>
2024-03-09 17:16:27 +1000

    Started work on chunked file transfer

--- src/file.c
+++ src/file.c
@@ -8,35 +8,7 @@
 #include "response.h"
 #include "http_status.h"
 #include "mime_types.h"
-
-// Read the files contents and send it to the client
-void send_file_contents(int client_socket, FILE *file_ptr)
-{
-    char buffer[FILE_BUFFER_SIZE];
-    size_t bytes_read;
-    size_t bytes_sent;
-
-    while ((bytes_read = fread(buffer, 1, FILE_BUFFER_SIZE, file_ptr)) > 0)
-    {
-        size_t total_sent = 0;
-        while (total_sent < bytes_read)
-        {
-            bytes_sent = send(client_socket, buffer + total_sent, bytes_read - total_sent, 0);
-            if (bytes_sent <= 0)
-            {
-                perror("Error sending file contents.");
-                fclose(file_ptr);
-                close(client_socket);
-                pthread_exit(NULL);
-            }
-
-            total_sent += bytes_sent;
-        }
-    }
-
-    // Close file pointer
-    fclose(file_ptr);
-}
+#include "config.h"
 
 // Send the file to the client.
 void send_file(int client_socket, char *file_path)
@@ -56,7 +28,7 @@ void send_file(int client_socket, char *file_path)
         if (file_ptr == NULL)
         {
             // TODO at this point I should send a 404
-            perror("Error opening requested file.");
+            perror("Error opening requested file");
             close(client_socket);
             pthread_exit(NULL);
         }
@@ -83,22 +55,27 @@ void send_file(int client_socket, char *file_path)
     get_status_message(response.status_code, response.status_message, sizeof(response.status_message));
 
     // Get mime type of the file
-    char mime_type[MAX_MIME_TYPE_SIZE];
+    char mime_type[MAX_MIME_TYPE_LENGTH];
     get_mime_type(file_path ,mime_type, sizeof(mime_type));
     add_header(&response, "Content-Type", mime_type);
 
     // Add content length header
     add_header(&response, "Content-Length", file_size_buffer);
 
+    // Add Transfer-Encoding header
+//    add_header(&response, "Transfer-Encoding", "chunked");
+
     // TODO add a flag to toggle adding this header or not
     // TODO define NovaWeb label and version somewhere
     // Add server header
     add_header(&response, "Server", "NovaWeb/0.1");
 
-    char output[MAX_RESPONSE_SIZE];
+    char output[MAX_RESPONSE_LENGTH];
     build_http_response(&response, output);
-    // TODO use debug flag to conditionally print response
-    print_response(response);
+    if (verbose_flag)
+    {
+        print_response(response);
+    }
 
     // Send the response
     size_t response_length = strlen(output);
@@ -106,11 +83,107 @@ void send_file(int client_socket, char *file_path)
     if (bytes_sent < response_length)
     {
         // TODO send a 500?
-        perror("Error sending response headers.");
+        perror("Error sending response headers");
         close(client_socket);
         pthread_exit(NULL);
     }
 
     // Send the file contents
     send_file_contents(client_socket, file_ptr);
+}
+
+// Read the files contents and send it to the client
+void send_file_contents(int client_socket, FILE *file_ptr)
+{
+    char buffer[FILE_BUFFER_SIZE];
+    size_t bytes_read, bytes_sent;
+
+    while ((bytes_read = fread(buffer, 1, FILE_BUFFER_SIZE, file_ptr)) > 0)
+    {
+        size_t total_sent = 0;
+        while (total_sent < bytes_read)
+        {
+            bytes_sent = send(client_socket, buffer + total_sent, bytes_read - total_sent, 0);
+            if (bytes_sent <= 0)
+            {
+                perror("Error sending file contents");
+                fclose(file_ptr);
+                close(client_socket);
+                pthread_exit(NULL);
+            }
+            total_sent += bytes_sent;
+        }
+    }
+
+    // Close file pointer
+    fclose(file_ptr);
+}
+
+// Read the files contents and send it to the client
+void send_chunked_file_contents(int client_socket, FILE *file_ptr)
+{
+    char buffer[FILE_BUFFER_SIZE];
+    size_t bytes_read;
+    size_t bytes_sent;
+
+    while ((bytes_read = fread(buffer, 1, FILE_BUFFER_SIZE, file_ptr)) > 0)
+    {
+        if (send_chunk(client_socket, buffer, bytes_read) == 0)
+        {
+            fclose(file_ptr);
+            close(client_socket);
+            pthread_exit(NULL);
+        }
+    }
+
+    // Send zero-size chunk to indicate end of file
+    if (send(client_socket, "0\r\n\r\n", 5, 0) < 0)
+    {
+        perror("Error sending zero-size chunk");
+    }
+
+    // Close file pointer
+    fclose(file_ptr);
+}
+
+// Send a chunk of data.
+int send_chunk(int client_socket, char *buffer, size_t size)
+{
+    char chunk_header[20];
+    ssize_t bytes_sent;
+    ssize_t total_sent = 0;
+
+    // Generate chunk header in hexadecimal format
+    sprintf(chunk_header, "%zx\r\n", size);
+
+    // Send chunk header
+    bytes_sent = send(client_socket, chunk_header, strlen(chunk_header), 0);
+    if (bytes_sent <= 0)
+    {
+        perror("Error sending chunk header");
+        return 0;
+    }
+
+    // Send chunk data
+    while (total_sent < size)
+    {
+        bytes_sent = send(client_socket, buffer + total_sent, size - total_sent, 0);
+        if (bytes_sent <= 0)
+        {
+            perror("Error sending file contents");
+            return 0;
+        }
+
+        total_sent += bytes_sent;
+    }
+
+    // Send end of chunk marker
+    bytes_sent = send(client_socket, "\r\n", 2, 0);
+    if (bytes_sent <= 0)
+    {
+        perror("Error sending chunk end marker");
+        return 0;
+    }
+
+    return 1;
 }
--- src/file.h
+++ src/file.h
@@ -5,7 +5,9 @@
 
 #define FILE_BUFFER_SIZE 4096
 
-void send_file_contents(int client_socket, FILE *file_ptr);
 void send_file(int client_socket, char *file_path);
+void send_file_contents(int client_socket, FILE *file_ptr);
+void send_chunked_file_contents(int client_socket, FILE *file_ptr);
+int send_chunk(int client_socket, char *buffer, size_t size);
 
 #endif //NOVAWEB_FILE_H
