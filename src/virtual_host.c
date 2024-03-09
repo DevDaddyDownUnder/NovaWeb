015ebc60fedecc255faa1c3d354c83088bf880d2
DevDaddyDownUnder <gossip_ponchos0g@icloud.com>
2024-03-09 10:19:43 +1000

    Add virtual host, used for storing and accessing the host and document root

---
+++ src/virtual_host.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "virtual_host.h"
+
+host_config hosts[MAX_HOSTS];
+int sizeof_hosts = 0;
+
+// Add a new host config.
+void add_host_config(char *host, char *document_root)
+{
+    if (sizeof_hosts > MAX_HOSTS)
+    {
+        fprintf(stderr, "Maximum number of hosts (%d) reached.\n", MAX_HOSTS);
+        return;
+    }
+
+    strncpy(hosts[sizeof_hosts].host, host, MAX_HOST_SIZE - 1);
+    strncpy(hosts[sizeof_hosts].document_root, document_root, MAX_DOCUMENT_ROOT_LENGTH - 1);
+    sizeof_hosts++;
+}
+
+// Return the document root for the specified host.
+char *get_document_root(char host[MAX_HOST_SIZE])
+{
+    for (int i = 0; i < sizeof_hosts; i++)
+    {
+        if (strcmp(host, hosts[i].host) == 0)
+        {
+            return hosts[i].document_root;
+        }
+    }
+
+    // Fallback to the directory where the program was executed
+    static char cwd[MAX_DOCUMENT_ROOT_LENGTH];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        return cwd;
+    }
+
+    perror("Unable to fallback to the directory where the program was executed. getcwd() error.");
+    return NULL;
+}
---
+++ src/virtual_host.h
@@ -0,0 +1,16 @@
+#ifndef NOVAWEB_VIRTUAL_HOST_H
+#define NOVAWEB_VIRTUAL_HOST_H
+
+#define MAX_HOSTS 10
+#define MAX_HOST_SIZE 1024
+#define MAX_DOCUMENT_ROOT_LENGTH 256
+
+typedef struct {
+    char host[MAX_HOST_SIZE];
+    char document_root[MAX_DOCUMENT_ROOT_LENGTH];
+} host_config;
+
+void add_host_config(char *host, char *document_root);
+char *get_document_root(char host[MAX_HOST_SIZE]);
+
+#endif //NOVAWEB_VIRTUAL_HOST_H
