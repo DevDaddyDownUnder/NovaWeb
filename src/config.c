2e6b9edcf7165186927f59335dcf0bbf94476647
DevDaddyDownUnder <gossip_ponchos0g@icloud.com>
2024-03-09 22:47:07 +1000

    Add directory listing flag

--- src/config.c
+++ src/config.c
@@ -2,3 +2,6 @@
 
 // Default verbose flag to false
 int verbose_flag = 0;
+
+// Default directory listing to true
+int directory_listing_flag = 1;
--- src/config.h
+++ src/config.h
@@ -2,5 +2,6 @@
 #define NOVAWEB_CONFIG_H
 
 extern int verbose_flag;
+extern int directory_listing_flag;
 
 #endif //NOVAWEB_CONFIG_H
