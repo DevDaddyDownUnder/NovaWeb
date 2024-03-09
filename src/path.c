bac4e0baf726bd107ba47ce7377b19ef3a713da0
DevDaddyDownUnder <gossip_ponchos0g@icloud.com>
2024-03-09 10:05:25 +1000

    Add path utilities, used for handling the requested file

---
+++ src/path.c
@@ -0,0 +1,60 @@
+#include <string.h>
+#include "path.h"
+
+void normalize_path(char *path)
+{
+    size_t length = strlen(path);
+
+    // Replace backslashes with forward slashes
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        if (path[i] == '\\')
+        {
+            path[i] = '/';
+        }
+    }
+
+    // Remove redundant slashes and trailing slash (except for root directory)
+    int j = 0;
+    for (i = 0; i < length; i++)
+    {
+        if (!(path[i] == '/' && (i + 1 < length && path[i + 1] == '/')))
+        {
+            path[j++] = path[i];
+        }
+    }
+
+    // Ensure the path is null-terminated
+    if (j > 1 && path[j - 1] == '/')
+    {
+        j--;
+    }
+
+    path[j] = '\0';
+}
+
+int build_file_path(char *file)
+{
+    // Normalize the file path
+    normalize_path(file);
+
+    // Validate requested file path, prevent directory traversal
+    if (strstr(file, "..") != NULL)
+    {
+        return 0;
+    }
+
+    // If root directory is requested, serve index.html
+    if (strcmp(file, "/") == 0)
+    {
+        strcpy(file, DEFAULT_FILE);
+    }
+    else
+    {
+        // Remove leading '/'
+        memmove(file, file + 1, strlen(file));
+    }
+
+    return 1;
+}
---
+++ src/path.h
@@ -0,0 +1,10 @@
+#ifndef NOVAWEB_PATH_H
+#define NOVAWEB_PATH_H
+
+#define DEFAULT_FILE "index.html"
+#define DEFAULT_FILE_EXTENSION ".html"
+
+void normalize_path(char *path);
+int build_file_path(char *requested_file);
+
+#endif //NOVAWEB_PATH_H
